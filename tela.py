2b66eefde057fdb94331429b069bda8e15a13a65
IanAlessandro <165028836+IanAlessandro@users.noreply.github.com>
2024-05-21 08:29:35 -0300

    Add files via upload

---
+++ tela.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+Window.size = (360, 640)
+
+class GerenciadorTelas(ScreenManager):
+    pass
+
+class PrimeiraTela(Screen):
+    pass
+   
+
+class SegundaTela(Screen):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return GerenciadorTelas()
+
+MyApp().run()
